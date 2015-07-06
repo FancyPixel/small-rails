100117d5494969cd29beb11bed03c4fbd4d3ab1d
Andrea Mazzini <andrea.mazzini@gmail.com>
2015-07-06 17:03:51 +0200

    Fix authentication

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ module V1
     
     # POST /v1/login
     def create
-      @user = User.find_for_database_authentication(email: params[:username])
+      @user = User.find_for_database_authentication(email: params[:email])
       return invalid_login_attempt unless @user
 
       if @user.valid_password?(params[:password])
